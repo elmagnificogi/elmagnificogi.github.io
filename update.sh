1e8543ecd9031a3bed88c1541d5aa5a5205ed3dc
elmagnificogi <elmagnificogg@gmail.com>
2015-11-03 13:52:37 +0800

    update-post

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 git add .;
 git commit -m "update-post";
-git remote rm origin£»
-git remote add origin https://github.com/elmagnificogi/elmagnificogi.github.io.git£»
+git remote rm origin;
+git remote add origin https://github.com/elmagnificogi/elmagnificogi.github.io.git;
 git push -u origin master
