520c362d2a36f7764b41d743d3776e087ddbaf87
magnifico el <magnifico el>
2016-12-24 21:11:32 +0800

    update-post

--- update.sh
+++ update.sh
@@ -3,5 +3,5 @@ unset https_proxy;
 git add .;
 git commit -m "update-post";
 git remote rm origin;
-git remote add origin https://github.com/elmagnificogi/elmagnificogi.github.io.git;
+git remote add origin https://github.com/elmagnificogi/http://elmagnificogi.github.io;
 git push -u origin master
