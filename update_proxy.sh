926a535f36e19bd6f680f26e99eda39803c6fdd1
magnifico el <magnifico el>
2016-12-24 15:13:06 +0800

    update-post

--- update_proxy.sh
+++ update_proxy.sh
@@ -3,5 +3,5 @@ export https_proxy="127.0.0.1:1080"
 git add .;
 git commit -m "update-post";
 git remote rm origin;
-git remote add origin https://github.com/elmagnificogi/elmagnificogi.github.io.git;
+git remote add origin https://github.com/elmagnificogi/huxblog-boilerplate1;
 git push -u origin master
