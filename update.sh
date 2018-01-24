6d21f538a80c0bdb1079aa1a67abca749e3c8a5d
elmagnificogi <elmagnificogg@gmail.com>
2018-01-24 14:23:54 +0800

    Modified the updata.sh

--- update.sh
+++ update.sh
@@ -2,6 +2,4 @@ unset http_proxy;
 unset https_proxy;
 git add .;
 git commit -m "update-post";
-git remote rm origin;
-git remote add origin https://github.com/elmagnificogi/elmagnificogi.github.io.git;
 git push -u origin master
--- update_proxy.sh
+++ update_proxy.sh
@@ -2,6 +2,4 @@ export http_proxy="127.0.0.1:1080"
 export https_proxy="127.0.0.1:1080"
 git add .;
 git commit -m "update-post";
-git remote rm origin;
-git remote add origin https://github.com/elmagnificogi/elmagnificogi.github.io.git;
 git push -u origin master
