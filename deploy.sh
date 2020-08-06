37ec9369c1d7be68fab98ef79bf4048beb550b0c
elmagnificoco <elmagnificogg@gmail.com>
2020-08-07 00:49:41 +0800

    Change deploy

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,8 @@ then
   exit 0
   #echo "`date '+%Y%m%d %H:%M'`:不需要更新代码"
 else
-  echo "`date '+%Y%m%d %H:%M'`:代码已经更新,开始build"
-  #jekyll build --source /root/elmagnificogi.github.io --destination /usr/share/nginx/html --incremental 
-  #ps -ef|grep jekyll | awk '{print $2}'| sed -n '1,1p' | xargs -i kill {}
+  echo "`date '+%Y%m%d %H:%M'`: post update,start build"
+  jekyll build --source /root/myblog --destination /home/wwwroot/example.com --incremental
+  echo "`date '+%Y%m%d %H:%M'`: build over"
   #rm -rf /usr/share/nginx/html
-  #jekyll serve -H '0.0.0.0' -s '/root/blog' >/dev/null 2>&1 &
 fi
