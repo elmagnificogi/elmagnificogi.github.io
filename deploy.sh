e7a8d028df28fb01017312bbb52cfd504ea8993a
elmagnifico <elmagnificogg@gmail.com>
2021-02-01 16:43:55 +0800

    update-post

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ if [[ "$result" != "" ]]
 then
   exit 0
 else
-  #echo "`date '+%Y%m%d %H:%M'`: post update,start build"
-  #result=$(jekyll build --source /root/elmagnificogi.github.io --destination /usr/share/nginx/html)
-  #echo $result
-  #echo "`date '+%Y%m%d %H:%M'`: build over"
+  echo "`date '+%Y%m%d %H:%M'`: post update,start build"
+  result=$(jekyll build --source /root/elmagnificogi.github.io --destination /usr/share/nginx/html)
+  echo $result
+  echo "`date '+%Y%m%d %H:%M'`: build over"
 fi
