3ce6d02fae006c4240bb8ba07e788f46dd589a78
elmagnifico <elmagnificogg@gmail.com>
2023-03-16 11:37:12 +0800

    update-post

--- js/sw-registration.js
+++ js/sw-registration.js
@@ -35,7 +35,7 @@ if(navigator.serviceWorker){
   // For security reasons, a service worker can only control the pages
   // that are in the same directory level or below it. That's why we put sw.js at ROOT level.
   navigator.serviceWorker
-    .register('/sw.js')
+    .register('/js/sw.js')
     .then((registration) => handleRegistration(registration))
     .catch((error) => {console.log('ServiceWorker registration failed: ', error)})
 
