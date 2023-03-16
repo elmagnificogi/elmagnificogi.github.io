e7a304feaa1443358245a2f86952a5defee50f5d
elmagnifico <elmagnificogg@gmail.com>
2023-03-16 10:34:22 +0800

    update-post

--- sw.js
+++ sw.js
@@ -27,9 +27,6 @@ const PRECACHE_LIST = [
 ]
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
-  "huangxuan.me",
-  "yanshuo.io",
-  "cdnjs.cloudflare.com"
 ]
 const DEPRECATED_CACHES = ['precache-v1', 'runtime', 'main-precache-v1', 'main-runtime']
 
